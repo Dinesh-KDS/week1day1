176d064d9f8f39fef2513dedf1c6f9b5bd8fa980
Dinesh-KDS <93469966+Dinesh-KDS@users.noreply.github.com>
2021-11-12 22:40:02 +0530

    Add files via upload

---
+++ ClassRoom.java
@@ -0,0 +1,29 @@
+package week1;
+
+public class ClassRoom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int num = 21;
+        
+        if (num%3 == 0); {
+			System.out.println("TRIZZ");
+			
+	    if (num%5 == 0);{
+	    	System.out.println("FRIZZ");
+	    	
+	    if (num%3 == 0 && num%5 == 0);{
+			System.out.println("TRIZZ-FRIZZ");
+			
+		
+		}
+				
+			}
+			
+		}
+			
+		}
+        	
+	}
+
+
