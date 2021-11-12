cf5100dbfe3cf1ef403024a074306d7e7b8e6892
Dinesh-KDS <93469966+Dinesh-KDS@users.noreply.github.com>
2021-11-12 22:39:08 +0530

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,22 @@
+package week1;
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+             
+         int first = 1;
+         int fact = 5;
+         
+         
+         for (int i = 1; i <= fact; i++){							
+					
+			
+			first = i * first;
+			
+		}
+         
+         System.out.println("factorial is " +first);
+	}
+
+}
