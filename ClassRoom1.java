66abfc07914e183e6560f77f7bb8b6128513b71f
Dinesh-KDS <93469966+Dinesh-KDS@users.noreply.github.com>
2021-11-12 22:41:35 +0530

    Add files via upload

---
+++ ClassRoom1.java
@@ -0,0 +1,19 @@
+package week1;
+
+public class ClassRoom1 {
+	// print all the even Numbers
+	public static void main(String[] args) {
+		
+		
+		for (int i = 0; i <= 69; i++) {
+			
+		     if (i%3==0) {
+				System.out.println(i + " is an odd Number");
+			}
+			
+		}
+		
+
+	}
+
+}
